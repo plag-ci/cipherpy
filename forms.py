3e02f6d84573771b6b21cb06b257dfbf54d11320
Ümit Dincel <ktumitt@gmail.com>
2015-05-17 02:16:29 +0300

    Something editing and patches

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ class CipherForm(Form):
 	key1 = TextField('form-control', validators=[DataRequired()])
 	key2 = TextField('form-control')
 	algorithm = SelectField('form-control', choices=[(0,'Ceasar'),(1,'Affine'),(6,'Additive'),(7,'Multiplicative'),(5,'Substitution'),(2,'Playfair'),(3,'Vigenere'),(4,'Autokey')])
-	switch = SelectField('form-control', choices=[(0,'decipher'),(1,'encipher')])
+	switch = SelectField('form-control', choices=[(1,'encipher'),(0,'decipher')])
 
 		#0 : ceasar, # key
 		#1 : affine, # key1 key2
--- main.py
+++ main.py
@@ -1,14 +1,50 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug import secure_filename
+
+from flask import Flask, render_template, request, redirect, url_for,make_response
 from forms import *
 from utils.monos import *
 from utils.poly import *
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt'])
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
 app.config.from_object('config')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/read', methods = ['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		file = request.files['file']
+		for line in file.readlines():
+			print line
+		# if file and allowed_file(file.filename):
+		# 	filename = secure_filename(file.filename)
+		# 	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		# 	return redirect(url_for('uploaded_file', filename=filename))
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form action="" method=post enctype=multipart/form-data>
+	<p><input type=file name=file>
+	<input type=submit value=Upload>
+	</form>
+	'''
+
+@app.route('/download-file', methods = [ 'GET', 'POST' ])
+def download_file():
+	response = make_response(request.form['the_file'].strip())
+	response.headers["Content-Disposition"] = "attachment; filename=cipher.txt"
+	return response
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-
 	options = {
 		0 : ceasar, # key
 		1 : affine, # key1 key2
@@ -23,12 +59,18 @@ def home():
 	form = CipherForm(csrf_enabled=True)
 	#Form Submit
 	if form.is_submitted():
-		text = form.text.data
+		text = ''
+		if request.files['file']:
+			for line in request.files['file'].readlines():
+				text += line
+		else:		
+			text = form.text.data
+
 		key1 = form.key1.data
 		key2 = form.key2.data
 		choice = int(form.algorithm.data)
 		switch = int(form.switch.data) # 0 decipher, 1 encipher
-		l_txt = options[choice](key1,key2,text,switch) 
+		l_txt = options[choice](key1,key2,text,switch)
 	else:
 		print 'oh shit'
 	return render_template('form-cipher.html', **locals())
